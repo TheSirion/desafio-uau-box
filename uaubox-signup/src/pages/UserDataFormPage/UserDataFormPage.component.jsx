6c86aacaca6b8fb09019b07c14394161b467e206
Matheus Ribeiro <thesirion@gmail.com>
2021-12-14 19:09:14 +0000

    improved styling of header, added forms to first page

--- uaubox-signup/src/pages/UserDataFormPage/UserDataFormPage.component.jsx
+++ uaubox-signup/src/pages/UserDataFormPage/UserDataFormPage.component.jsx
@@ -8,7 +8,24 @@ const UserDataFormPage = () => (
       <Breadcrumb />
     </Header>
     <main>
+      <h1>Você está quase lá!</h1>
+      <form>
+        <label for="nome">Nome</label>
+        <input type="text" name="nome" />
 
+        <label for="sobrenome">Sobrenome</label>
+        <input type="text" name="sobrenome" />
+        
+        <label for="cpf">CPF</label>
+        <input type="text" name="cpf" />
+        
+        <label for="data-nascimento">Data de nascimento</label>
+        <input type="text" name="data-nascimento" />
+        
+        <label for="telefone">Telefone</label>
+        <input type="text" name="telefone" />
+        
+      </form>
     </main>
   </>
 )
