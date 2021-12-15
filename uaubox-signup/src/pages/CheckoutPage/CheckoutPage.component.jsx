d610b977e7aa86b925ee0acb44885f5f081fba0b
Matheus Ribeiro <thesirion@gmail.com>
2021-12-15 18:09:51 +0000

    finished setting up 7-1 architecture, created Button component

--- uaubox-signup/src/pages/CheckoutPage/CheckoutPage.component.jsx
+++ uaubox-signup/src/pages/CheckoutPage/CheckoutPage.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Breadcrumb from '../../components/Breadcrumb/Breadcrumb.component';
+import Button from "../../components/Button/Button.component";
 import Header from "../../components/Header/Header.component";
 
 const CheckoutPage = () => (
@@ -8,7 +9,29 @@ const CheckoutPage = () => (
       <Breadcrumb />
     </Header>
     <h1>Resumo da compra</h1>
-
+    <div className="wrapper">
+      <div className="form">
+        <div className="form__group">
+          <label for="cep">CEP</label>
+          <input type="text" name="cep" />
+          <Button>Buscar</Button>
+        </div>
+        <div className="form__group">
+          <label for="endereco">Endereço</label>
+          <input type="text" name="endereco" disabled />
+        </div>
+        <div className="form__group">
+          <label for="numero">Número</label>
+          <input type="text" name="cep" />
+          <label for="complemento">Complemento</label>
+          <input type="text" name="cep" />
+        </div>
+      </div>
+      <div className="subscription">
+        <h2>Assinatura de R$89,90</h2>
+        <h2>Frete Gratis</h2>
+      </div>
+    </div>
   </>
 )
 
--- uaubox-signup/src/pages/UserDataFormPage/UserDataFormPage.component.jsx
+++ uaubox-signup/src/pages/UserDataFormPage/UserDataFormPage.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Breadcrumb from '../../components/Breadcrumb/Breadcrumb.component';
+import Button from "../../components/Button/Button.component";
 import Header from '../../components/Header/Header.component';
 
 const UserDataFormPage = () => (
@@ -26,8 +27,8 @@ const UserDataFormPage = () => (
         <label for="telefone">Telefone</label>
         <input type="text" name="telefone" />
 
-        <Link to="/">Voltar</Link>
-        <Link to="/Checkout">Continuar</Link>
+        <Link to="/"><Button>Voltar</Button></Link>
+        <Link to="/checkout"><Button>Continuar</Button></Link>
       </form>
     </main>
   </>
