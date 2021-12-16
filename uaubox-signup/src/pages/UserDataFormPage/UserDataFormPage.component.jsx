166dc05724e709bbc9defa9244749ab2841613fa
Matheus Ribeiro <thesirion@gmail.com>
2021-12-16 01:57:55 +0000

    improved styling of userDataFormPage

--- uaubox-signup/src/pages/UserDataFormPage/UserDataFormPage.component.jsx
+++ uaubox-signup/src/pages/UserDataFormPage/UserDataFormPage.component.jsx
@@ -10,25 +10,47 @@ const UserDataFormPage = () => (
       <Breadcrumb />
     </Header>
     <main>
-      <h1 className='h1'>Você está quase lá!</h1>
-      <form>
-        <label htmlFor="nome">Nome</label>
-        <input type="text" name="nome" />
+      <div className="title">
+        <h1 className='h1'>Você está quase lá!</h1>
+      </div>
+      <form className='form'>
+        <div className="form__group">
+          <div className='form__input-wrapper'>
+            <label className="form__input-label" htmlFor="nome">Nome</label>
+            <input className="form__input" type="text" name="nome" />
+          </div>
+          <div className='form__input-wrapper'>
+            <label className="form__input-label" htmlFor="sobrenome">Sobrenome</label>
+            <input className="form__input" type="text" name="sobrenome" />
+          </div>
+        </div>
 
-        <label htmlFor="sobrenome">Sobrenome</label>
-        <input type="text" name="sobrenome" />
+        <div className="form__group">
+          <div className='form__input-wrapper'>
+            <label className="form__input-label" htmlFor="cpf">CPF</label>
+            <input className="form__input" type="text" name="cpf" />
+          </div>
+          <div className='form__input-wrapper'>
+            <label className="form__input-label" htmlFor="data-nascimento">Data de nascimento</label>
+            <input className="form__input" type="text" name="data-nascimento" />
+          </div>
+        </div>
 
-        <label htmlFor="cpf">CPF</label>
-        <input type="text" name="cpf" />
+        <div className="form__group">
+          <div className='form__input-wrapper'>
+            <label className="form__input-label" htmlFor="telefone">Telefone</label>
+            <input className="form__input" type="text" name="telefone" />
+          </div>
+        </div>
 
-        <label htmlFor="data-nascimento">Data de nascimento</label>
-        <input type="text" name="data-nascimento" />
-
-        <label htmlFor="telefone">Telefone</label>
-        <input type="text" name="telefone" />
-
-        <Link to="/"><Button>Voltar</Button></Link>
-        <Link to="/checkout"><Button>Continuar</Button></Link>
+        <div className="form__buttons">
+          <Link to="/">
+            <Button className="btn btn--secondary">Voltar</Button>
+          </Link>
+          <Link to="/checkout">
+            <Button className="btn btn--primary">Continuar</Button>
+          </Link>
+        </div>
       </form>
     </main>
   </>
